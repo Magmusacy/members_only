84a4179fcef712dfdb06dfb09d68adf06a143517
Magmusacy <xpfk.098@gmail.com>
2022-02-12 18:48:25 +0100

    Create my own authentication/session system

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?(user)
+    true if user && session[:user_id] == user.id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root "posts#index"
 end
