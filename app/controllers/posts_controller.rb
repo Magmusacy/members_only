dd119740883447837160cbf42eb521cd400dcf97
Magmusacy <xpfk.098@gmail.com>
2022-02-12 15:44:07 +0100

    Create simple showcase of posts and added CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,52 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Congratulations! Post created successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Something went wrong..."
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Congratulations! Post updated successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Something went wrong..."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    flash[:notice] = "Congratulations! Post deleted successfully."
+      redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { in: 3..10 }
+  validates :body, presence: true, length: { in: 1..500 }
 end
