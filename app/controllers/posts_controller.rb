984b948ccbd3ced307dcec37de5dec8b0c2c2ddd
Magmusacy <xpfk.098@gmail.com>
2022-02-12 19:45:10 +0100

    Require login before Post and show correct actions for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :require_login, except: [:index, :show]
+
   def index
     @posts = Post.all
   end
@@ -8,7 +10,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    # This because current_user is currently signed in user so we can use this association to include user's id in post
+    @post = helpers.current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post, notice: "Congratulations! Post created successfully."
@@ -23,11 +26,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = helpers.current_user.posts.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = helpers.current_user.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to @post, notice: "Congratulations! Post updated successfully."
@@ -38,7 +41,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    @post = helpers.current_user.posts.find(params[:id])
     @post.delete
     redirect_to root_path, notice: "Congratulations! Post deleted successfully."
   end
@@ -47,5 +50,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def require_login
+    unless helpers.current_user
+      redirect_to root_path, alert: "You need to sign up in order to do that"
+    end
   end
 end
